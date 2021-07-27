290acb0f4093e6a64d5ed48300a78eda5eedcbbd
Vlad Olaru <vlad@pixelgrade.com>
2021-07-27 13:29:08 +0300

    Done with the Opcache handling - no need for WordPress plugin to handle the opcache

--- src/Cache/CacheDispatcher.php
+++ src/Cache/CacheDispatcher.php
@@ -23,6 +23,7 @@ use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use PixelgradeLT\Conductor\Queue\QueueInterface;
 use PixelgradeLT\Conductor\Queue\ActionQueue;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Class to dispatch events related to site-cache(s).
@@ -44,7 +45,7 @@ class CacheDispatcher {
 	 * Handle Composer events and fire actions accordingly.
 	 *
 	 * This is intended to be called from Composer scripts and receive Composer events instances.
-	 * @see https://getcomposer.org/doc/articles/scripts.md#event-classes
+	 * @see   https://getcomposer.org/doc/articles/scripts.md#event-classes
 	 *
 	 * @since 0.9.0
 	 *
@@ -55,18 +56,32 @@ class CacheDispatcher {
 			return;
 		}
 
-		// Load everything we need.
-		self::load_wp( self::determine_site_root_path( $event ) );
+		// Make sure that everything we need is loaded.
+		try {
+			self::load_wp( self::determine_site_root_path( $event ) );
+		} catch ( \Exception $e ) {
+			// Bail.
+			return;
+		}
 
 		switch ( $event->getName() ) {
 			case PackageEvents::PRE_PACKAGE_INSTALL:
+				// This package didn't exist prior, so no need to do anything.
+				break;
 			case PackageEvents::PRE_PACKAGE_UPDATE:
 			case PackageEvents::PRE_PACKAGE_UNINSTALL:
+				// Before changing the package files, invalidate the opcache for the current files,
+				// so we don't lose files that might be deleted.
 				self::invalidate_package_opcache( $event );
 				break;
 			case PackageEvents::POST_PACKAGE_INSTALL:
+				// This is a new package that should be compiled.
+				self::compile_package_opcache( $event );
+				self::schedule_cache_clear( $event );
+				break;
 			case PackageEvents::POST_PACKAGE_UPDATE:
 			case PackageEvents::POST_PACKAGE_UNINSTALL:
+				// No need to compile changes since we've invalidated before package change.
 				self::schedule_cache_clear( $event );
 				break;
 		}
@@ -74,60 +89,113 @@ class CacheDispatcher {
 	}
 
 	/**
-	 * Clear the opcache after a package event (install, update, uninstall).
+	 * Compile the opcache after a package event (install, update, uninstall).
 	 *
 	 * This is intended to be called from Composer scripts and receive Composer events instances.
-	 * @see https://getcomposer.org/doc/articles/scripts.md#event-classes
+	 * @see   https://getcomposer.org/doc/articles/scripts.md#event-classes
+	 *
+	 * @since 0.9.0
+	 *
+	 * @param Event|null $event
+	 */
+	public static function compile_package_opcache( Event $event = null ) {
+		if ( ! $event instanceof PackageEvent || ! method_exists( $event->getOperation(), 'getPackage' ) ) {
+			return;
+		}
+
+		// Make sure that everything we need is loaded.
+		try {
+			self::load_wp( self::determine_site_root_path( $event ) );
+		} catch ( \Exception $e ) {
+			// Bail.
+			return;
+		}
+
+		$package             = $event->getOperation()->getPackage();
+		$installationManager = $event->getComposer()->getInstallationManager();
+		$packagePath         = $installationManager->getInstallPath( $package );
+		if ( empty( $packagePath ) ) {
+			return;
+		}
+
+		try {
+			$cachetool = self::get_cachetool();
+			if ( ! $cachetool->extension_loaded( 'Zend OPcache' ) ) {
+				return;
+			}
+			$splFiles = self::prepareFileList( $packagePath );
+		} catch ( \Exception $e ) {
+			// Bail.
+			return;
+		}
+
+		foreach ( $splFiles as $file ) {
+			$cachetool->opcache_compile_file( $file->getRealPath() );
+		}
+	}
+
+	/**
+	 * Invalidate the opcache after a package event (install, update, uninstall).
+	 *
+	 * This is intended to be called from Composer scripts and receive Composer events instances.
+	 * @see   https://getcomposer.org/doc/articles/scripts.md#event-classes
 	 *
 	 * @since 0.9.0
 	 *
 	 * @param Event|null $event
 	 */
 	public static function invalidate_package_opcache( Event $event = null ) {
-		if ( ! $event instanceOf PackageEvent || ! method_exists( $event->getOperation(), 'getPackage' ) ) {
+		if ( ! $event instanceof PackageEvent || ! method_exists( $event->getOperation(), 'getPackage' ) ) {
 			return;
 		}
 
-		self::load_wp( self::determine_site_root_path( $event ) );
+		// Make sure that everything we need is loaded.
+		try {
+			self::load_wp( self::determine_site_root_path( $event ) );
+		} catch ( \Exception $e ) {
+			// Bail.
+			return;
+		}
 
-		$package = $event->getOperation()->getPackage();
+		$package             = $event->getOperation()->getPackage();
 		$installationManager = $event->getComposer()->getInstallationManager();
-		$packagePath = $installationManager->getInstallPath($package);
+		$packagePath         = $installationManager->getInstallPath( $package );
 		if ( empty( $packagePath ) ) {
 			return;
 		}
 
-		$cachetool = self::get_cachetool();
-		$info = $cachetool->opcache_get_status(true);
-		if ( empty( $info['scripts']) ) {
-			// Opcache is either disabled or there are no scripts cached.
+		try {
+			$cachetool = self::get_cachetool();
+			if ( ! $cachetool->extension_loaded( 'Zend OPcache' ) ) {
+				return;
+			}
+			$info = $cachetool->opcache_get_status( true );
+			if ( empty( $info['scripts'] ) ) {
+				// Opcache is either disabled or there are no scripts cached.
+				return;
+			}
+		} catch ( \Exception $e ) {
+			// Bail.
 			return;
 		}
 
-		switch ( $event->getOperation()->getOperationType() ) {
-			case InstallOperation::TYPE:
-			case UpdateOperation::TYPE:
-			case UninstallOperation::TYPE:
-				foreach ($info['scripts'] as $script) {
-					if ( empty( $script['full_path'] ) ) {
-						continue;
-					}
+		foreach ( $info['scripts'] as $script ) {
+			if ( empty( $script['full_path'] ) ) {
+				continue;
+			}
 
-					if (preg_match('|' . $packagePath . '|', $script['full_path'])) {
-						$cachetool->opcache_invalidate( $script['full_path'] );
-					}
-				}
-				break;
-			default:
-				break;
+			if ( 0 === strpos( $script['full_path'], $packagePath ) ) {
+				$cachetool->opcache_invalidate( $script['full_path'] );
+			}
 		}
 	}
 
 	protected static function get_cachetool() {
 		$adapter = null;
-		if ( defined('LT_PHP_FCGI_HOST') && ! empty( LT_PHP_FCGI_HOST ) ) {
-			$adapter = new FastCGI(LT_PHP_FCGI_HOST );
+		if ( defined( 'LT_PHP_FCGI_HOST' ) && ! empty( LT_PHP_FCGI_HOST ) ) {
+			$adapter = new FastCGI( LT_PHP_FCGI_HOST );
 		}
+
 		return CacheTool::factory( $adapter );
 	}
 
@@ -138,14 +206,20 @@ class CacheDispatcher {
 	 * with multiple instructions to clear the cache at the same time.
 	 *
 	 * This is intended to be called from Composer scripts and receive Composer events instances.
-	 * @see https://getcomposer.org/doc/articles/scripts.md#event-classes
+	 * @see   https://getcomposer.org/doc/articles/scripts.md#event-classes
 	 *
 	 * @since 0.9.0
 	 *
 	 * @param Event|null $event
 	 */
 	public static function schedule_cache_clear( Event $event = null ) {
-		self::load_wp( self::determine_site_root_path( $event ) );
+		// Make sure that everything we need is loaded.
+		try {
+			self::load_wp( self::determine_site_root_path( $event ) );
+		} catch ( \Exception $e ) {
+			// Bail.
+			return;
+		}
 
 		if ( ! self::get_queue()->get_next( 'pixelgradelt_conductor/clear_site_cache' ) ) {
 			$context = [];
@@ -160,7 +234,11 @@ class CacheDispatcher {
 						$context['event']['package_type'] = $event->getOperation()->getPackage()->getType();
 
 						// We only schedule for certain package types, not all.
-						if ( ! in_array( $context['event']['package_type'], [ 'wordpress-plugin', 'wordpress-theme', 'wordpress-core', ] ) ) {
+						if ( ! in_array( $context['event']['package_type'], [
+							'wordpress-plugin',
+							'wordpress-theme',
+							'wordpress-core',
+						] ) ) {
 							return;
 						}
 					}
@@ -182,7 +260,7 @@ class CacheDispatcher {
 	protected static function determine_site_root_path( Event $event = null ) {
 		$root_path = '';
 		if ( $event && method_exists( $event, 'getComposer' ) ) {
-			$root_path = dirname( $event->getComposer()->getConfig()->get('vendor-dir' ) );
+			$root_path = dirname( $event->getComposer()->getConfig()->get( 'vendor-dir' ) );
 		}
 
 		if ( empty( $root_path ) ) {
@@ -190,6 +268,27 @@ class CacheDispatcher {
 		}
 
 		return $root_path;
+	}
+
+	/**
+	 * Get a list of files searched recursively in the path, possibly excluding some of them.
+	 *
+	 * @param string $path Absolute path to the directory to search in.
+	 * @param array  $exclude Patterns to exclude by. See https://symfony.com/doc/current/components/finder.html (the notPath part).
+	 *
+	 * @return \Traversable|\SplFileInfo[]
+	 */
+	protected static function prepareFileList( $path, $exclude = [] ) {
+		return Finder::create()
+		             ->files()
+		             ->in( $path )
+		             ->name( '*.php' )
+		             ->notPath( '/Tests/' )
+		             ->notPath( '/tests/' )
+		             ->notPath( $exclude )
+		             ->ignoreUnreadableDirs()
+		             ->ignoreDotFiles( true )
+		             ->ignoreVCS( true );
 	}
 
 	protected static function get_queue() {
